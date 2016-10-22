b221d4cc5d8fa1b7e2649c4c471cadd8cc6aeee9
Zain-Ali <ZainAli@Zains-MacBook-Pro.local>
2016-10-22 19:11:53 +0100

    Another unnecessary function

--- TeachingMySelfPython.py
+++ TeachingMySelfPython.py
@@ -2,5 +2,12 @@ def HelloWorld():
 	for i in range (0 , 10):
 		print("Hello, World")
 
+def AddNumber():
+	FirstNumber = 1
+	SecondNumber = 11
+	Total = FirstNumber + SecondNumber
+	print (Total)
+	
 if __name__ == '__main__':
 	HelloWorld()
+	AddNumbers():
