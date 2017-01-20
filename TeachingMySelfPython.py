2c3bd42acb434f7a90a6c2170b6a569188f58e61
Zain-Ali <zain@Eduroam-17-9280.d.port.ac.uk>
2017-01-20 11:30:01 +0000

    Testing Graphics Py Library

--- TeachingMySelfPython.py
+++ TeachingMySelfPython.py
@@ -1,8 +1,18 @@
+from graphics import*
+
+def main():
+	win = GraphWin("First Graphics Window", 500, 500)
+	c = Circle (Point(100, 100), 50)
+	c.draw(win)
+	win.getMouse()
+	win.close()
+main()
+
 def HelloWorld():
 	for i in range (0 , 10):
 		print("Hello, World")
 
-def AddNumber():
+def AddNumbers():
 	FirstNumber = 1
 	SecondNumber = 11
 	Total = FirstNumber + SecondNumber
@@ -10,4 +20,4 @@ def AddNumber():
 	
 if __name__ == '__main__':
 	HelloWorld()
-	AddNumbers():
+	AddNumbers()
